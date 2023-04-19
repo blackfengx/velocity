b0092e6f0474fd65c77a2ae362f0e4b6c7bb01ea
blackfengx <blackfengsm@gmail.com>
2023-04-19 15:22:23 -0500

    test

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -3,7 +3,7 @@ function MainPage() {
     <div className="px-4 py-5 my-5 text-center">
       <h1 className="display-5 fw-bold">CarCar</h1>
       <div>
-        <h2></h2>
+        <h2>test</h2>
           <img className="img-thumbnail" src="https://pictures.dealer.com/t/toyotadallasvtg/1488/e91a3dfb21391614dbcad8cd3da874a7x.jpg"/>
       </div>
     </div>
